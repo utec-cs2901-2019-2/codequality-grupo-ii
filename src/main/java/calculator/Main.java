d69a8d8c6201e118657861cead67509632ead776
anggieyc <anggie.yengle@utec.edu.pe>
2019-10-16 13:08:12 -0500

    fin

--- src/main/java/calculator/Main.java
+++ src/main/java/calculator/Main.java
@@ -3,12 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-    String operation;
-    Scanner obj=new Scanner(System.in);
-    operation=obj.nextLine();
-    int arr[]=new int[operation.length()];
-    Op o=new Op(operation);
-    float v=o.solve(0,operation.length()-1);
-    System.out.println(v);
+        operation op = new operation("1.0+2.0");
+        System.out.println(op.operate());
     }
 }
